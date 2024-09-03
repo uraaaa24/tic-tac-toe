ce66cddbdc378b63d37b9f51ca621dfd3faf21cb
Yuto Urashima <yuto.urashima24@gmail.com>
2024-09-03 11:04:07 +0900

    refactor: Add onSquareClick prop to Square component

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,5 +1,17 @@
-const Square = ({ value, className }: { value: string; className?: string }) => {
-  return <button className={`border-gray-400 p-2 h-16 w-16 text-4xl ${className}`}>{value}</button>
+const Square = ({
+  value,
+  onSquareClick,
+  className
+}: {
+  value: string
+  onSquareClick: () => void
+  className?: string
+}) => {
+  return (
+    <button onClick={onSquareClick} className={`border-gray-400 p-2 h-16 w-16 text-4xl ${className}`}>
+      {value}
+    </button>
+  )
 }
 
 export default Square
