69297aa028cde9481035ff0d6ab67c3b05b21fba
Yuto Urashima <yuto.urashima24@gmail.com>
2024-09-03 11:04:00 +0900

    refactor: Implement click handling for squares in Board component

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,17 @@
+'use client'
+
+import { useState } from 'react'
 import Square from './Square'
 
 const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
+  function handleSquareCLick(i: number) {
+    const newSquares = squares.slice()
+    newSquares[i] = 'X'
+    setSquares(newSquares)
+  }
+
   return (
     <div className="grid grid-cols-3 w-48 h-48">
       {Array.from({ length: 9 }).map((_, i) => {
@@ -9,13 +20,14 @@ const Board = () => {
         return (
           <Square
             key={i}
-            value={value}
+            value={squares[i]}
+            onSquareClick={() => handleSquareCLick(i)}
             className={`
-            ${i < 3 ? 'border-t-2' : ''}
-            ${i % 3 === 0 ? 'border-l-2' : ''}
-            ${i % 3 === 2 ? 'border-r-2' : ''}
-            ${i > 5 ? 'border-b-2' : ''}
-            border-r-2 border-b-2
+              ${i < 3 ? 'border-t-2' : ''}
+              ${i % 3 === 0 ? 'border-l-2' : ''}
+              ${i % 3 === 2 ? 'border-r-2' : ''}
+              ${i > 5 ? 'border-b-2' : ''}
+              border-r-2 border-b-2
             `}
           />
         )
